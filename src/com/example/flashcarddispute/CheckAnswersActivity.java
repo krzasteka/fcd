4a02e522d27110c3fd0db49c66a25b24ec881f37
Andrew Krzastek <krzasteka@gmail.com>
2014-04-24 23:07:13 -0400

    refute button is working, not crashing when going to next activity

--- src/com/example/flashcarddispute/CheckAnswersActivity.java
+++ src/com/example/flashcarddispute/CheckAnswersActivity.java
@@ -41,8 +41,6 @@ public class CheckAnswersActivity extends Activity {
  		});
 	}
 	
-	
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
