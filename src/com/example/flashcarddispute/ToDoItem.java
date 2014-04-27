2122ccb8d613a65ebe164339539f16bb3a8696b9
Andrew Krzastek <krzasteka@gmail.com>
2014-04-27 14:27:31 -0400

    Starting new refute activity

--- src/com/example/flashcarddispute/ToDoItem.java
+++ src/com/example/flashcarddispute/ToDoItem.java
@@ -1,87 +1,87 @@
-package com.example.flashcarddispute;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import android.content.Intent;
-
-// Do not modify 
-
-public class ToDoItem {
-
-	public static final String ITEM_SEP = System.getProperty("line.separator");
-
-	
-
-	public final static String TITLE = "title";
-	
-	public final static String DATE = "date";
-	public final static String FILENAME = "filename";
-
-	public final static SimpleDateFormat FORMAT = new SimpleDateFormat(
-			"yyyy-MM-dd HH:mm:ss", Locale.US);
-
-	private String mTitle = new String();
-
-	private Date mDate = new Date();
-
-	ToDoItem(String title,  Date date) {
-		this.mTitle = title;
-		this.mDate = date;
-	}
-
-	// Create a new ToDoItem from data packaged in an Intent
-
-	ToDoItem(Intent intent) {
-
-		mTitle = intent.getStringExtra(ToDoItem.TITLE);
-	
-		try {
-			mDate = ToDoItem.FORMAT.parse(intent.getStringExtra(ToDoItem.DATE));
-		} catch (ParseException e) {
-			mDate = new Date();
-		}
-	}
-
-	public String getTitle() {
-		return mTitle;
-	}
-
-	public void setTitle(String title) {
-		mTitle = title;
-	}
-
-	
-	
-
-	public Date getDate() {
-		return mDate;
-	}
-
-	public void setDate(Date date) {
-		mDate = date;
-	}
-
-	// Take a set of String data values and 
-	// package them for transport in an Intent
-
-	public static void packageIntent(Intent intent, String title, String date) {
-
-		intent.putExtra(ToDoItem.TITLE, title);
-		intent.putExtra(ToDoItem.DATE, date);
-	
-	}
-
-	public String toString() {
-		return mTitle + ITEM_SEP +  ITEM_SEP 
-				+ FORMAT.format(mDate);
-	}
-
-	public String toLog() {
-		return "Title:" + mTitle + ITEM_SEP  + "Date:"
-				+ FORMAT.format(mDate);
-	}
-
-}
+//package com.example.flashcarddispute;
+//
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//import java.util.Locale;
+//
+//import android.content.Intent;
+//
+//// Do not modify 
+//
+//public class ToDoItem {
+//
+//	public static final String ITEM_SEP = System.getProperty("line.separator");
+//
+//	
+//
+//	public final static String TITLE = "title";
+//	
+//	public final static String DATE = "date";
+//	public final static String FILENAME = "filename";
+//
+//	public final static SimpleDateFormat FORMAT = new SimpleDateFormat(
+//			"yyyy-MM-dd HH:mm:ss", Locale.US);
+//
+//	private String mTitle = new String();
+//
+//	private Date mDate = new Date();
+//
+//	ToDoItem(String title,  Date date) {
+//		this.mTitle = title;
+//		this.mDate = date;
+//	}
+//
+//	// Create a new ToDoItem from data packaged in an Intent
+//
+//	ToDoItem(Intent intent) {
+//
+//		mTitle = intent.getStringExtra(ToDoItem.TITLE);
+//	
+//		try {
+//			mDate = ToDoItem.FORMAT.parse(intent.getStringExtra(ToDoItem.DATE));
+//		} catch (ParseException e) {
+//			mDate = new Date();
+//		}
+//	}
+//
+//	public String getTitle() {
+//		return mTitle;
+//	}
+//
+//	public void setTitle(String title) {
+//		mTitle = title;
+//	}
+//
+//	
+//	
+//
+//	public Date getDate() {
+//		return mDate;
+//	}
+//
+//	public void setDate(Date date) {
+//		mDate = date;
+//	}
+//
+//	// Take a set of String data values and 
+//	// package them for transport in an Intent
+//
+//	public static void packageIntent(Intent intent, String title, String date) {
+//
+//		intent.putExtra(ToDoItem.TITLE, title);
+//		intent.putExtra(ToDoItem.DATE, date);
+//	
+//	}
+//
+//	public String toString() {
+//		return mTitle + ITEM_SEP +  ITEM_SEP 
+//				+ FORMAT.format(mDate);
+//	}
+//
+//	public String toLog() {
+//		return "Title:" + mTitle + ITEM_SEP  + "Date:"
+//				+ FORMAT.format(mDate);
+//	}
+//
+//}
